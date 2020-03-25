49ad7450d304164eeb94da0275dc453ed915a73f
destinyson7 <tanish.lad@research.iiit.ac.in>
2020-03-25 23:24:23 +0530

    updated latest coefficients

--- client_moodle.py
+++ client_moodle.py
@@ -12,7 +12,7 @@ MAX_DEG = 11
 
 def get_errors(id, vector):
     """
-    returns python array of length 2 
+    returns python array of length 2
     (train error and validation error)
     """
     for i in vector:
@@ -55,13 +55,13 @@ def send_request(id, vector, path):
 
 if __name__ == "__main__":
     """
-    Replace "test" with your secret ID and just run this file 
+    Replace "test" with your secret ID and just run this file
     to verify that the server is working for your ID.
     """
 
-    # err = get_errors('se1Poy6HllKuLEK3WlsQnfi6qAN6zt5JqbUgbchBylLc0FmRf2', [0.0, 8.512477622723912, 2.3928925150630524e-13, 0.04543092418673353, -1.3595698932163956e-13, -2.2869608191664645e-10, -7.439202914623583e-14, -2.2004472741453184e-10, -2.1444592822341143e-13, -3.671092218312515e-13, 0.0])
-    # assert len(err) == 2
-    # print(err)
+    err = get_errors('se1Poy6HllKuLEK3WlsQnfi6qAN6zt5JqbUgbchBylLc0FmRf2', [0.0, 8.512477622723912, 2.3928925150630524e-13, 0.04543092418673353, -1.3595698932163956e-13, -2.2869608191664645e-10, -7.439202914623583e-14, -2.2004472741453184e-10, -2.1444592822341143e-13, -3.671092218312515e-13, 0.0])
+    assert len(err) == 2
+    print(err)
 
-    submit_status = submit('se1Poy6HllKuLEK3WlsQnfi6qAN6zt5JqbUgbchBylLc0FmRf2', [-1.3343118857515203e-13, 8.512477622723642, 1.876613640451281e-13, 0.045430924186980595, -1.9690443115476632e-13, -2.2857423188609912e-10, 1.769119047547387e-13, -2.1996322289374318e-10, -6.17486137177974e-13, -2.9176408381119876e-13, 0.0])
+    submit_status = submit('se1Poy6HllKuLEK3WlsQnfi6qAN6zt5JqbUgbchBylLc0FmRf2', [-2.2415160708773027e-14, 8.512477622724045, 3.060450949278663e-13, 0.04543092418673353, -4.304501471232224e-13, -2.2891866690052717e-10, -3.599867283156126e-13, -2.2004472741453184e-10, -6.17486137177974e-13, -2.9176408381119876e-13, 0.0])
     print(submit_status)
--- genetic.py
+++ genetic.py
@@ -3,7 +3,7 @@ import random
 import json
 
 k = 10
-num_generations = 150
+num_generations = 4
 
 secret_key = 'se1Poy6HllKuLEK3WlsQnfi6qAN6zt5JqbUgbchBylLc0FmRf2'
 
@@ -19,7 +19,7 @@ small_random_prob = 0.63
 initial_coefficients = json.load(open("coefficients.txt"))
 init = []
 
-min_error = 1e12
+min_error = (1e12, 1e12)
 best_coeff = []
 errors = []
 
@@ -27,20 +27,20 @@ ratio = 1
 mutation_range = 1e-13
 
 for i in initial_coefficients:
-    if min_error > float(abs(initial_coefficients[i][0] - initial_coefficients[i][1])):
-        min_error = float(abs(initial_coefficients[i][0] - initial_coefficients[i][1]))
+    if min_error > (float(abs(initial_coefficients[i][0] - ratio * initial_coefficients[i][1]) * 5 + abs(initial_coefficients[i][0] + ratio * initial_coefficients[i][1])), float(abs(initial_coefficients[i][0] + ratio * initial_coefficients[i][1]))):
+        min_error = (float(abs(initial_coefficients[i][0] - ratio * initial_coefficients[i][1]) * 5 + abs(initial_coefficients[i][0] + ratio * initial_coefficients[i][1])), float(abs(initial_coefficients[i][0] + ratio * initial_coefficients[i][1])))
 
         best_coeff = i.strip('][').split(', ')
         both_errors = (initial_coefficients[i][0], initial_coefficients[i][1])
 
     init.append(list(map(float, i.strip('][').split(', '))))
 
-    errors.append((float(abs(initial_coefficients[i][0] - initial_coefficients[i][1])), list(map(
+    errors.append(((float(abs(initial_coefficients[i][0] - ratio * initial_coefficients[i][1]) * 5 + abs(initial_coefficients[i][0] + ratio * initial_coefficients[i][1])), float(abs(initial_coefficients[i][0] + ratio * initial_coefficients[i][1]))), list(map(
         float, i.strip('][').split(', '))), (initial_coefficients[i][0], initial_coefficients[i][1])))
 
 while len(init) < k:
     init.append(list(map(float, best_coeff)))
-    errors.append((float(min_error), list(map(float, best_coeff)), both_errors))
+    errors.append((min_error, list(map(float, best_coeff)), both_errors))
 
 # print(errors)
 
@@ -99,7 +99,7 @@ def crossover(child1, child2):
 for i in range(num_generations):
     err = [get_errors(secret_key, state[j]) for j in range(k)]
 
-    fitness = [abs(err[j][0] - err[j][1]) for j in range(k)]
+    fitness = [(abs(err[j][0] - ratio * err[j][1]) * 5 + err[j][0] + ratio * err[j][1], abs(err[j][0] + ratio * err[j][1])) for j in range(k)]
     # fitness = [(ratio * err[j][1]) for j in range(k)]
 
     # print(i, errors)
@@ -116,10 +116,10 @@ for i in range(num_generations):
 
     errors = [new_errors[j] for j in range(k)]
 
-    const = 100 / (1 / fitness[0] + 1 / fitness[1] +
-                   1 / fitness[2] + 1 / fitness[3])
+    const = 100 / (1 / fitness[0][0] + 1 / fitness[1][0] +
+                   1 / fitness[2][0] + 1 / fitness[3][0])
 
-    perc = [const / fitness[j] for j in range(k)]
+    perc = [const / fitness[j][0] for j in range(k)]
 
     new_state = random.choices(state, perc, k=k)
     # print(new_state)
